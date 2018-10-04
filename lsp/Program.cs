0d0649723bc202a3899495486ae085f14e4f1b98
bsi90586 <bsi90586@LOC3768>
2018-10-04 18:08:35 -0300

    removido propriedades nao utilzadas

--- lsp/Program.cs
+++ lsp/Program.cs
@@ -4,9 +4,6 @@ namespace lsp
 {
     public class Rectangle
     {
-        //public int Width { get; set; }
-        //public int Height { get; set; }
-
         public virtual int Width { get; set; }
         public virtual int Height { get; set; }
 
